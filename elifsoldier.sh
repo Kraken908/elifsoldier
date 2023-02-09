521eff263a64fa7f5b553edef201f1125f4db887
kraken <70107358+Kraken908@users.noreply.github.com>
2023-02-09 14:14:51 -0700

    Create elifsoldier.sh

---
+++ elifsoldier.sh
@@ -0,0 +1,13 @@
+#!/bin/bash                                                                     
+read -p " Do you want to quit soldier?!" answer                                 
+if [ "$answer" == yes ]                                                         
+ then                                                                           
+                                                                                
+    echo "You don't easily give up soldier"                                     
+                                                                                
+elif [ "$answer" == "no" ]; then                                                
+                                                                                
+    echo " Now that's a good soldier!"                                          
+fi                                                                              
+                                                                                
+# Let's have fun with some Linux scripts !    \m/ ("") \m/      
